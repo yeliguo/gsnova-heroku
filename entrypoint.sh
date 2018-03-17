dca2e4c2a1e857b6347cf3e88d12e7985266dc34
wangyi2005 <wangyi2005@users.noreply.github.com>
2018-03-17 20:06:34 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 cd /gsnova
-wget http://github.com/yinqiwen/gsnova/releases/download/$VER/gsnova_linux_amd64-$VER.tar.bz2 | tar xj
+wget http://github.com/yinqiwen/gsnova/releases/download/$VER/gsnova_linux_amd64-$VER.tar.bz2 
+bzip2 -d gsnova_linux_amd64-$VER.tar.bz2
 chmod +x gsnova 
 ./gsnova -cmd -server -http :$PORT -window $Window -window_refresh $Window_Refresh -user "*" -log console
