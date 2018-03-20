870dc16aa2eae4ed01027d5e322dcb25177de181
wangyi2005 <wangyi2005@users.noreply.github.com>
2018-03-21 00:22:45 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 cd /gsnova
-if [["$VER" > "0.31.0" ]]; then 
+if [[ "$VER" > "0.31.0" ]]; then 
    wget -O gsnova.tar.bz2 http://github.com/yinqiwen/gsnova/releases/download/$VER/gsnova_linux_amd64-$VER.tar.bz2 
    tar -xjvf gsnova.tar.bz2
    chmod +x gsnova 
   ./gsnova -cmd -server -http :$PORT -key $KEY -window $Window -window_refresh $Window_Refresh -user "*" -log console
-elif [["$VER" = "0.31.0" ]]; then 
+elif [[ "$VER" = "0.31.0" ]]; then 
    wget -O gsnova.tar.bz2 http://github.com/yinqiwen/gsnova/releases/download/$VER/gsnova_server_linux_amd64-$VER.tar.bz2 
    tar -xjvf gsnova.tar.bz2
    chmod +x gsnova_server
