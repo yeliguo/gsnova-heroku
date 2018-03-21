9be86def3a466c288fbdfd06a465148366ab1012
wangyi2005 <wangyi2005@users.noreply.github.com>
2018-03-21 18:38:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,5 @@
 cd /gsnova
-if [[ "$VER" > "0.31.0" ]]; then 
-   wget -O gsnova.tar.bz2 "http://github.com/yinqiwen/gsnova/releases/download/$VER/gsnova_linux_amd64-$VER.tar.bz2" 
-   tar -xjvf gsnova.tar.bz2
-   chmod +x gsnova 
-  ./gsnova -cmd -server -http :$PORT -key $KEY -window $Window -window_refresh $Window_Refresh -user "*" -log console
-elif [[ "$VER" = "0.31.0" ]]; then 
-   wget -O gsnova.tar.bz2 "http://github.com/yinqiwen/gsnova/releases/download/$VER/gsnova_server_linux_amd64-$VER.tar.bz2" 
-   tar -xjvf gsnova.tar.bz2
-   chmod +x gsnova_server
-   ./gsnova_server -http :$PORT -key $KEY -user "*" -log console
-else
-   wget -O gsnova.tar.bz2 "http://github.com/yinqiwen/gsnova/releases/download/v$VER/gsnova_server_linux_amd64-v$VER.tar.bz2" 
-   tar -xjvf gsnova.tar.bz2
-   chmod +x gsnova_server
-   ./gsnova_server -http :$PORT -key $KEY -user "*" -log console
-fi 
+wget -O gsnova.tar.bz2 http://github.com/yinqiwen/gsnova/releases/download/$VER/gsnova_linux_amd64-$VER.tar.bz2 
+tar -xjvf gsnova.tar.bz2
+chmod +x gsnova 
+./gsnova -cmd -server -http :$PORT -key $KEY -window $Window -window_refresh $Window_Refresh -user "*" -log console
